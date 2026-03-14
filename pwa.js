e72742a1df8cc46cafe0812eed992fd38ed6025c
CHAKRI-GUPTHA <chakriguptha123@gmail.com>
2026-03-14 16:38:31 +0530

    Clear old service worker cache

--- pwa.js
+++ pwa.js
@@ -1,7 +1,20 @@
-﻿if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.getRegistrations().then((regs) => {
-      regs.forEach((reg) => reg.unregister());
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      const regs = await navigator.serviceWorker.getRegistrations();
+      await Promise.all(regs.map((reg) => reg.unregister()));
+
+      if ("caches" in window) {
+        const keys = await caches.keys();
+        await Promise.all(keys.map((key) => caches.delete(key)));
+      }
+
+      if (!sessionStorage.getItem("sw-cleared")) {
+        sessionStorage.setItem("sw-cleared", "1");
+        window.location.reload();
+      }
+    } catch (err) {
+      console.warn("Service worker cleanup failed", err);
+    }
   });
 }
